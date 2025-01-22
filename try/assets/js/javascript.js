d81e8b65560f404e57a7b62546b9c8b71d44eef1
JahidDesign <jahidhasan290113@gmail.com>
2025-01-22 21:56:30 +0600

    first commit

---
+++ try/assets/js/javascript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var nav = document.getElementById("navId");
+    if (nav.style.display === "none") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+  }
+  
